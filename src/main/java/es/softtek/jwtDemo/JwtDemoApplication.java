560b9a882501df9dcc8212733570e3afcd25f223
jose gorostiaga <gorostiagajosemaria@hotmail.com>
2022-04-09 12:28:29 -0300

    1°

--- src/main/java/es/softtek/jwtDemo/JwtDemoApplication.java
+++ src/main/java/es/softtek/jwtDemo/JwtDemoApplication.java
@@ -2,15 +2,21 @@ package es.softtek.jwtDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JwtDemoApplication {
+public class JwtDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
 	}
-	
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JwtDemoApplication.class);
+	}
 
 }
 
